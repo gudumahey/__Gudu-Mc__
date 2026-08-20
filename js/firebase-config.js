80aee2ff85fe3cc2e9649709b01057b0dda7fe27
gudumahey <maheys337@gmail.com>
2026-08-20 14:23:01 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+
+    apiKey: "AIzaSyA9GCAmfofP7Yg8Yfq5WgvnPzpZ_Yd4Vf8",
+
+    authDomain: "gudu-mc.firebaseapp.com",
+
+    projectId: "gudu-mc",
+
+    storageBucket: "gudu-mc.firebasestorage.app",
+
+    messagingSenderId: "284955450310",
+
+    appId: "1:284955450310:web:7c74a1de4a2475707570c7",
+
+    measurementId: "G-RP47XF158M"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+
+export {
+    auth,
+    googleProvider,
+    signInWithPopup
 };
